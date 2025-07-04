7e930c3b11264bb94cd8e47b5ec4f8749f5be099
장정안 <wjddks96@naver.com>
2025-07-04 23:07:06 +0900

    feat: 0704 프로그래머스 모의고사

--- 2025-07/0704/0704.js
+++ 2025-07/0704/0704.js
@@ -1,1 +1,49 @@
-console.log('이건 예시');
+function solution(answers) {
+  const firstPerson = () => {
+    const rule = [1, 2, 3, 4, 5];
+    const results = [];
+    for (let i = 0; i < 10000; i++) {
+      results.push(rule[i % rule.length]);
+    }
+    return results;
+  };
+
+  const secondPerson = () => {
+    const rule = [2, 1, 2, 3, 2, 4, 2, 5];
+    const results = [];
+    for (let i = 0; i < 10000; i++) {
+      results.push(rule[i % rule.length]);
+    }
+    return results;
+  };
+
+  const thirdPerson = () => {
+    const rule = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    const results = [];
+    for (let i = 0; i < 10000; i++) {
+      results.push(rule[i % rule.length]);
+    }
+    return results;
+  };
+
+  let count1 = 0;
+  let count2 = 0;
+  let count3 = 0;
+
+  for (let i = 0; i < answers.length; i++) {
+    const first = firstPerson();
+    const second = secondPerson();
+    const third = thirdPerson();
+
+    if (first[i] === answers[i]) count1++;
+    if (second[i] === answers[i]) count2++;
+    if (third[i] === answers[i]) count3++;
+  }
+  const maxCount = Math.max(count1, count2, count3);
+  const answer = [];
+  if (count1 === maxCount) answer.push(1);
+  if (count2 === maxCount) answer.push(2);
+  if (count3 === maxCount) answer.push(3);
+
+  return answer;
+}
