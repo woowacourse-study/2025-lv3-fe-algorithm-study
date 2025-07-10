92292fc889c94c5ff18e718972532a29eef11f79
minji2219 <aabb0219@naver.com>
2025-07-10 22:23:50 +0900

    N과M(8)

---
+++ 2025-07/0710/15657.js
@@ -0,0 +1,26 @@
+const [nm, arr] = require('fs')
+  .readFileSync('input.txt')
+  .toString()
+  .split('\n');
+
+const [n, m] = nm.split(' ').map(Number);
+const array = arr.split(' ').map(Number);
+array.sort((a, b) => a - b);
+
+const newArr = [];
+const answer = [];
+
+dfs(0);
+console.log(answer.join('\n'));
+
+function dfs(start) {
+  if (newArr.length === m) {
+    answer.push(newArr.join(' '));
+    return;
+  }
+  for (let i = start; i < n; i++) {
+    newArr.push(array[i]);
+    dfs(i);
+    newArr.pop();
+  }
+}
