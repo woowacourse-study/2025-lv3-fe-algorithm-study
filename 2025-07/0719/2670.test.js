75a251e2246ad293dd031d4f9ce3efa747064516
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-07-19 23:44:54 +0900

    feat: [0719] 2670번 문제 해결

--- 2025-07/0719/2670.test.js
+++ 2025-07/0719/2670.test.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+function run() {
+    const N = Number(input[0]);
+    let temp = 1;
+    let res = 0;
+    for (let i=1; i<=N; i++) {
+        number = Number(input[i]);
+        temp *= number;
+        if (number > temp) temp = number;
+        if (temp > res) res = temp;
+    }
+    console.log(res.toFixed(3));
+}
+
+run();
+
+
+test("run", () => {
+    expect(run(8, [1.1, 0.7, 1.3, 0.9, 1.4, 0.8, 0.7, 1.4]).toBe());
+});
+
+// 1.1
+// 1.1 * 0.7 (비교) 0.7 -> 결정
+// 결정된 것 (비교) 
 // 비교 1.3
