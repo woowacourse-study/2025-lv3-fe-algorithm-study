c048f4caaf47dea4181bf2999c39a741f81499be
minji2219 <aabb0219@naver.com>
2025-08-10 14:47:07 +0900

    다른 풀이

--- 2025-08/0809/10974.js
+++ 2025-08/0809/10974.js
@@ -1,36 +1,55 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const n = Number(require('fs').readFileSync('input.txt').toString().trim());
+let array = new Array(n).fill(0).map((_, idx) => idx + 1);
 
-const N = Number(input);
+let visited = new Array(n).fill(false);
+let newArr = [];
 
-let visited = Array(N).fill(false);
-
-let selected = Array(N).fill(0);
-
-let answer = '';
-
-function dfs(depth) {
-  if (depth === N) {
-    let result = [];
-    for (let i = 0; i < N; i++) {
-      result.push(selected[i]);
-    }
-
-    answer += result.join(' ') + '\n';
+function dfs() {
+  if (newArr.length === n) {
+    console.log(newArr.join(' '));
+    return;
   }
-
-  for (let i = 1; i <= N; i++) {
-    if (visited[i]) {
-      continue;
-    }
-
-    selected[depth] = i;
+  for (let i = 0; i < n; i++) {
+    if (visited) continue;
     visited[i] = true;
-    dfs(depth + 1);
+    newArr.push(array[i]);
+    dfs();
     visited[i] = false;
+    newArr.pop();
   }
 }
+dfs();
 
-dfs(0);
+// const reverseArr = [...array].sort((a, b) => b - a);
 
-console.log(answer.trim());
+// let answer = '';
+// answer += array.join(' ') + '\n';
+
+// while (array.join(' ') !== reverseArr.join(' ')) {
+//   let index = 0;
+//   for (let i = n - 1; i >= 0; i--) {
+//     if (array[i] > array[i - 1]) {
+//       index = i - 1;
+//       break;
+//     }
+//   }
+
+//   let min = 8;
+//   let minIndex = 0;
+//   for (let i = index + 1; i < n; i++) {
+//     if (array[index] < array[i] && array[i] < min) {
+//       min = array[i];
+//       minIndex = i;
+//     }
+//   }
+
+//   let temp = array[index];
+//   array[index] = array[minIndex];
+//   array[minIndex] = temp;
+
+//   let sortArr = array.splice(index + 1).sort((a, b) => a - b);
+//   array = [...array, ...sortArr];
+
+//   answer += array.join(' ') + '\n';
+// }
+// console.log(answer.trim());
