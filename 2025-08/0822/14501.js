1c89bed28fcee1d7040d6aa8e0cf085fa895c461
minji2219 <aabb0219@naver.com>
2025-08-22 00:12:09 +0900

    퇴사

---
+++ 2025-08/0822/14501.js
@@ -0,0 +1,23 @@
+const [n, ...arr] = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .split('\n');
+const N = Number(n);
+const counsel = arr.map((tp) => tp.split(' ').map(Number));
+
+function solution(n, counsel) {
+  const DP = new Array(N).fill(0);
+
+  for (let i = 0; i < n; i++) {
+    const [period, profit] = counsel[i];
+    if (i + period > N) continue;
+    DP[i] = DP[i] + profit;
+
+    for (let j = i + period; j < N; j++) {
+      DP[j] = Math.max(DP[j], DP[i]);
+    }
+  }
+  return Math.max(...DP);
+}
+
+console.log(solution(N, counsel));
